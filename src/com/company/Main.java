f899e2329d06d855c63a8056db3e4977c37d371c
jorgereina1986 <jorgereina1986@gmail.com>
2015-11-07 22:39:31 -0500

    now completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,7 @@ public class Main
 
     public static void main(String[] args)
     {
-        System.out.println(isEven("aabbccdd"));
-        System.out.println(isEven("abcdabcd"));
-        System.out.println(isEven("llooll"));
+        System.out.println(isEven("tattletale"));
 
     }
 
